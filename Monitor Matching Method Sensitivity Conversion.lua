9f20df8376af030600784914cd67e7efbb159581
Fahim Faisal <83073365+Voyager0001@users.noreply.github.com>
2021-10-17 17:11:15 +0600

    Create Monitor Matching Method Sensitivity Conversion.lua

---
+++ Monitor Matching Method Sensitivity Conversion.lua
@@ -0,0 +1,9 @@
+function mmm(ratio, sens, firstFOV, secondFOV) 
+  fov0=firstFOV* 0.5 * math.pi / 180
+  fov1 = secondFOV * 0.5 * math.pi / 180
+  cm360_1=100
+  alpha0 = math.atan(ratio * math.tan(fov0))
+  alpha1 = math.atan(ratio * math.tan(fov1))
+  cm360_2 = cm360_1 * alpha0 / alpha1
+  newSens=sens*(cm360_2/100)
+end
